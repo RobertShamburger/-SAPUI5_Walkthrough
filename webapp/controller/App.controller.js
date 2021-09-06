306d754edfbd60a1e4e8e4565d78a2979b997092
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2021-09-06 16:22:58 +0200

    Step 19

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,12 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ], function (Controller) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
-	});
+    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
+        onOpenDialog : function () {
+            this.getOwnerComponent().openHelloDialog();
+        }
+    });
 
 });
