75173b9e486c6edfa2680d3db55089ff1bd1f402
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2021-09-07 13:52:52 +0200

    Update formatter.js

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -7,7 +7,7 @@ sap.ui.define([], function () {
                 case "A":
                     return resourceBundle.getText("invoiceStatusA");
                 case "B":
-                    return resourceBundle.getText("invoiceStatusB");                case "B":
+                    return resourceBundle.getText("invoiceStatusB");
                 case "C":
                     return resourceBundle.getText("invoiceStatusC");
                 default:
