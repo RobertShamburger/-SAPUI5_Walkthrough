2c64ad0848b67296a18cd8b8914bd2728df4a3a6
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2021-09-09 17:14:26 +0200

    Update InvoiceList.controller.js

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -1,10 +1,10 @@
 sap.ui.define([
-        "sap/ui/core/mvc/Controller",
-        "sap/ui/model/json/JSONModel",
-        "../model/formatter",
-        "sap/ui/model/Filter",
-        "sap/ui/model/FilterOperator"
-    ], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
         formatter: formatter,
