12e2764be127fb03b69a898f44da777f35c2113b
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2021-09-09 12:53:16 +0200

    Updated "Compare" to step 32

--- compare/Component.js
+++ compare/Component.js
@@ -29,6 +29,9 @@ sap.ui.define([
 			this.rootControlLoaded().then(function() {
 				this._helloDialog = new HelloDialog(this.getRootControl());
 			}.bind(this));
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
 		},
 
 		exit : function () {
