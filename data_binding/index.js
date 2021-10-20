260bf5ca0e32f9436e024c6a56eecbd6a8fb2e68
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2021-10-20 16:19:10 +0200

    Step 4

--- data_binding/index.js
+++ data_binding/index.js
@@ -8,12 +8,20 @@ sap.ui.require([
 	sap.ui.getCore().attachInit(function () {
         // Create a JSON model from an object literal
         var oModel = new JSONModel ({
+            firstName: "Mike",
+            lastName: "Hawk",
+            enabled: true,
+            panelHeaderText: "Data Binding Basics"
             greetingText: "Hi, my name is Mike Hawk"
         });
         // Assign the model object to the SAPUI5 core
         sap.ui.getCore().setModel(oModel);
+        // Display the XML view called "App"
+        new XMLView({
+            viewName: "sap.ui.demo.db.view.App"
+        }).placeAt("content");
         // Display a text element whose text is derived
         // from the model object
-		new Text({text: "{/greetingText}"}).placeAt("content");
+//new Text({text: "{/greetingText}"}).placeAt("content");
 	});
 });
