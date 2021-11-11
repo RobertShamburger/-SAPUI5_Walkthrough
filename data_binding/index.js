5ca5fe2764b801e36896c29151ead32b901442d6
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2021-11-11 14:58:46 +0100

    Step 6 Data Bondage

--- data_binding/index.js
+++ data_binding/index.js
@@ -11,13 +11,27 @@ sap.ui.require([
         var oModel = new JSONModel({
             firstName: "Mike",
             lastName: "Hawk",
-            enabled: true,
-            panelHeaderText: "Data Binding Basics"
+            enabled: true
         });
-        oModel.setDefaultBindingMode(BindingMode.OneWay);
-        
         // Assign the model object to the SAPUI5 core
         sap.ui.getCore().setModel(oModel);
+        // Create a resource bundle for language specific texts
+        // the configured supportedLocales represent the i18n files present:
+        // * "" - i18n/i18n.properties
+        // the configured fallbackLocale should represent one of these files
+        // * "" - according to the fallback chain the root bundle is the last fallback.
+        // Configuring it explicitly avoids side effects when additional resource files are added.
+        
+        // oModel.setDefaultBindingMode(BindingMode.OneWay);
+        
+        var oResourceModel = new ResourceModel({
+            bundleName: "sap.ui.demo.data_binding.i18n.i18n",
+            supportedLocales: ["", "de"],
+            fallbackLocale: ""
+        });
+                
+        // Assign the model object to the SAPUI5 core
+        sap.ui.getCore().setModel(oResourceModel, "i18n");
         // Display the XML view called "App"
        new XMLView({
            viewName: "sap.ui.demo.data_binding.view.App"
